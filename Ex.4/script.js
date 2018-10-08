4dba306826601c49862b8a7105386d2c6b5e0ad8
Леонид <l.bratslavsky@neurobotics.ru>
2018-10-08 13:02:20 +0300

    Дз 3

---
+++ Ex.4/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var button = document.getElementById('button');
+document.getElementById('check-response').addEventListener("click", function () {
+    // Формируем ajax запрос
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://127.0.0.1:8080/result.json');
+
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Парсим json в объекты
+                var items = JSON.parse(xhr.responseText);
+                console.log(items);
+                // Проходимся по всем объектам
+                items.forEach(function (item) {
+                    console.log(item.result);
+                    if (item.result === "success") {
+                        button.style.backgroundColor = 'green';
+                    } else {
+                        button.style.backgroundColor = 'red';
+                    }
+                });
+            }
+        }
+    }
+});
